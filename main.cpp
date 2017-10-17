6e099565ab0d368060b9935c4aabdcc066b59fbd
Hugo Valle <hugovalle1@weber.edu>
2017-10-17 12:49:07 -0600

    Add doxygen documentation

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ int main()
     Counter c1;
     Counter c2(99);
     CountDn c3;
+    CountDn c4(50);
     cout<<"C1 Count is: "<<c1.getCount()<<endl;
     ++c2;
     cout<<"C2 Count is: "<<c2.getCount()<<endl;
-
     ++c3;
     ++c3;
     --c3;
     cout<<"C3 Count is: "<<c3.getCount()<<endl;
+    cout<<"C4 Count is: "<<c4.getCount()<<endl;
     return 0;
 }
